4c376f0f7f57d747f6887fb6488ec49f1a3e625d
Taka Masa <takahashi@dptr.jp>
2018-10-24 15:15:50 +0900

    timezoneを東京に

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Kintai
     # i18n対応
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
