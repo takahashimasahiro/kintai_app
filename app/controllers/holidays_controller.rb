53c33819835863e41c22e89a08bcb205071e1b49
Taka Masa <takahashi@dptr.jp>
2018-11-07 19:19:26 +0900

    ユーザーの有休状況を取得

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,4 +1,9 @@
 class HolidaysController < ApplicationController
   def show
+    @my_vacation = @current_user.apply_vacations.all.order(:get_start_date)
+  end
+  def edit 
+  end
+  def update
   end
 end
