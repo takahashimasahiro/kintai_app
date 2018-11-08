6632bb39d5cc8b449a46a01627750cf636520ca9
Taka Masa <takahashi@dptr.jp>
2018-11-08 18:47:21 +0900

    有給休暇申請の一覧を表示

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -3,7 +3,11 @@ class HolidaysController < ApplicationController
     @my_vacation = @current_user.apply_vacations.all.order(:get_start_date)
   end
   def edit 
+    # @applying_vacation = ApplyVacation.where(status: 'applying').order(:get_start_date)
+    @applying_vacation = User.joins(:apply_vacations).select("apply_vacations.* , users.name ").order("apply_vacations.get_start_date")
+    # a = ApplyVacation.joins(:users).select("users.*, apply_vacations.* ").where("apply_vacations.applicant_id == users.id").first
   end
+  
   def update
   end
 end
--- app/helpers/holidays_helper.rb
+++ app/helpers/holidays_helper.rb
@@ -1,2 +1,17 @@
 module HolidaysHelper
+  module HolidaysConstant
+    APPLICATION_STATUS = [%w(applying 申請中), %w(boss_applied 上長承認済), %w(admin_applied 管理者承認済), %w(withdrawal 申請取下), %w(apply_rejection 不許可)].map(&:freeze).freeze
+  end
+
+  def application_status
+    HolidaysConstant.freeze
+    HolidaysConstant::APPLICATION_STATUS
+  end
+
+  def select_status(status)
+    HolidaysConstant.freeze
+    val = nil
+    HolidaysConstant::APPLICATION_STATUS.each{ |r| r[0] == status ? val = r[1] : val }
+    val
+  end
 end
