51156b7310d9957086ca168f71c4c69539996f36
Taka Masa <takahashi@dptr.jp>
2018-11-09 15:55:31 +0900

    spec記載

--- spec/controllers/holidays_controller_spec.rb
+++ spec/controllers/holidays_controller_spec.rb
@@ -27,4 +27,27 @@ RSpec.describe HolidaysController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe ' #update' do
+  params = {
+    id: 1,
+    user_id: 1,
+    get_date: Date.today,
+    approve: nil,
+    dismiss: nil
+  }
+    it '申請許可' do
+      params[:approve] = ""
+      params[:dismiss] = nil
+      patch :update, params: params
+      expect(response).to have_http_status '302'
+    end
+
+    it '申請却下' do
+      params[:approve] = nil
+      params[:dismiss] = ""
+      patch :update, params: params
+      expect(response).to have_http_status '302'
+    end
+  end
 end
