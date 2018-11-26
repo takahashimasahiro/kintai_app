70f8b71872cc865dbdc4f3382f7d30a05c3cbcd1
Taka Masa <takahashi@dptr.jp>
2018-11-26 19:15:05 +0900

    rubocopのEを解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
-
-  # https://railsguides.jp/association_basics.html
   has_many :attendance_times, dependent: :destroy
   has_many :apply_vacations, foreign_key: 'applicant_id', dependent: :destroy
 
@@ -21,6 +19,6 @@ class User < ApplicationRecord
 
   # 選択したユーザーの情報を取得する
   def self.select_user(user_id)
-    User.find(id: user_id || @current_user.id) ? @current_user.owner?
+    User.find(id: user_id || @current_user.id) if @current_user.owner?
   end
 end
