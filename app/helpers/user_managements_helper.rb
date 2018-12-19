e8b422061bd13d007f8baa6e572119ec0ae5fb92
Taka Masa <takahashi@dptr.jp>
2018-12-19 18:23:49 +0900

    rubocopに教えを乞う

--- app/helpers/user_managements_helper.rb
+++ app/helpers/user_managements_helper.rb
@@ -1,8 +1,8 @@
 module UserManagementsHelper
   ROLE = {
-    owner:         I18n.t(:role_status, scope: [:activerecord, :attributes, :user])[0],
-    employee:      I18n.t(:role_status, scope: [:activerecord, :attributes, :user])[1],
-    part_time_job: I18n.t(:role_status, scope: [:activerecord, :attributes, :user])[2]
+    owner:         I18n.t(:role_status, scope: %i[activerecord attributes user])[0],
+    employee:      I18n.t(:role_status, scope: %i[activerecord attributes user])[1],
+    part_time_job: I18n.t(:role_status, scope: %i[activerecord attributes user])[2]
   }.freeze
 
   def all_role
