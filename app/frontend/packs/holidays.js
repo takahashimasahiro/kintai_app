9f1325c6222e1c7d2b354867ff196886c9e49100
Taka Masa <takahashi@dptr.jp>
2018-12-26 18:44:39 +0900

    休暇申請処理変更

--- app/frontend/packs/holidays.js
+++ app/frontend/packs/holidays.js
@@ -7,14 +7,22 @@ import $ from 'jquery';
 $(function () {
   $('button[name=dismiss]').on('click', function () {
     if (confirm('却下しますか？')) {
-      $('input[name=button]')[0].value = 'apply_rejection'
-      $(this).submit()
+      let row = $(this).parent().parent()[0].className
+      $(`input[class=button_${row}]`)[0].value ='apply_rejection'
+      addComment(row, $(`tr[class=${row}]`).children('th').children('input[name=owner-comment]')[0].value)
     } else {
       return false
     }
   })
   $('button[name=approve]').on('click', function () {
-    $('input[name=button]')[0].value = 'admin_applied'
-    $(this).submit()
+    let row = $(this).parent().parent()[0].className
+    $(`input[class=button_${$(this).parent().parent()[0].className}]`)[0].value ='admin_applied'
+    addComment(row, $(`tr[class=${row}]`).children('th').children('input[name=owner-comment]')[0].value)
+  })
+  $('input[name=owner-comment]').on('change', function(){
+    addComment($(this).parent().parent()[0].className, $(this)[0].value)
   })
 })
+function addComment(row, val){
+  $(`input[class=comment_${row}]`)[0].value = val
+}
