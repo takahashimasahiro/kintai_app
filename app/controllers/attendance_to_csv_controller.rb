10414d9c2863fe341f19bfb06fdf3d9ecab12d2b
Taka Masa <takahashi@dptr.jp>
2019-01-09 18:14:08 +0900

    不要なコメントアウトを削除

--- app/controllers/attendance_to_csv_controller.rb
+++ app/controllers/attendance_to_csv_controller.rb
@@ -6,10 +6,6 @@ class AttendanceToCsvController < ApplicationController
     @first_month = AttendanceTime.first_month(params[:select_year], params[:select_month])
     @all_user = User.all
     @selected_user = User.select_user(1, @current_user)
-    # # @attend_data @all_user.first.attendance_times.where(work_date: @first_month.all_month).order(:work_date)
-    # @all_attend_data = User.find(@selected_user.id).attendance_times.where(work_date: @first_month.all_month).order(:work_date)
-    # # 有休申請数
-    # @vacation_count = @selected_user.vacation_count_for_month(@first_month) || 0
     # ファイル名変更
     respond_to do |f|
       f.csv do
